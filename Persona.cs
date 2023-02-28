5a111a3eb4ce5f37c242998af89e20614a53b4df
Renato221 <renatocastrocastaneda13@gmail.com>
2023-02-28 15:53:50 -0600

    Clase Program y clase persona

---
+++ Persona.cs
@@ -0,0 +1,16 @@
+namespace Ejercicio
+
+class Persona {
+
+    public int edad;
+
+    public void saludar() {
+        
+        Console.WriteLine("Hola");
+    }
+    public void SetEdad(int edad) {
+
+        this.edad = edad;
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
